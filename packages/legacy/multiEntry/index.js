623ac02dfae2bbd6032195e68c785caa8b0a0c08
Tommy Troy Lin <tommytroylin@gmail.com>
2018-08-16 10:43:21 +0800

    fix: copy path issue

--- packages/legacy/multiEntry/index.js
+++ packages/legacy/multiEntry/index.js
@@ -58,8 +58,22 @@ module.exports = /** @type {import('@nowa/core').Types.ISolution} */ ({
       }),
       [
         ['file', ({ options }) => ({ type: 'empty', from: options.outputPath })],
-        ['file', ({ context, options }) => ({ type: 'copy', from: path.resolve(context, 'html'), to: options.outputPath })],
-        ['file', ({ context, options }) => ({ type: 'copy', from: path.resolve(context, 'src/lib'), to: options.outputPath })],
+        [
+          'file',
+          ({ context, options }) => ({
+            type: 'copy',
+            from: path.resolve(context, 'html'),
+            to: `${options.outputPath}${options.outputPath.slice(-1) === '/' ? '' : '/'}`,
+          }),
+        ],
+        [
+          'file',
+          ({ context, options }) => ({
+            type: 'copy',
+            from: path.resolve(context, 'src/lib'),
+            to: `${options.outputPath}${options.outputPath.slice(-1) === '/' ? '' : '/'}`,
+          }),
+        ],
         ({ options }) => (options.locale.length > 1 ? ['webpack', path.resolve(__dirname, './webpack/prodOL.js')] : undefined),
         ['webpack', path.resolve(__dirname, './webpack/prod.js')],
       ],
